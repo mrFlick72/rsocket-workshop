a9dd40b80a53b77ecaf552b001df09d8e40f31ea
Valerio Vaudi <valerio.vaudi@lastminute.com>
2020-01-22 14:06:21 +0100

    first commit

---
+++ consumer/src/test/java/it/valeriovaudi/rsocket/workshop/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.valeriovaudi.rsocket.workshop.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ publisher/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/PublisherApplication.java
@@ -0,0 +1,13 @@
+package it.valeriovaudi.rsocket.workshop.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherApplication.class, args);
+	}
+
+}
