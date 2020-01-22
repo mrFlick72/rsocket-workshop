fa18ca346b9dfa25e5a3c22bed462fec9525eac6
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-22 23:02:39 +0100

    tip

--- publisher/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/RequestReplyUseCase.java
+++ publisher/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/RequestReplyUseCase.java
@@ -4,6 +4,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
+/*
+TODO
+Implements a simple echo that once received a message republish the inbound message as response
+TIPS think to RSocket to messages to exchange and think how Spring manage typically a situation like this.
+For instance think how Spring manage websocket
+ */
 @Controller
 public class RequestReplyUseCase {
 
