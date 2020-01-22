f708526f357204f2e6c2cd1f3b3d5b35c7bf9935
Valerio Vaudi <valerio.vaudi@lastminute.com>
2020-01-22 17:42:08 +0100

    requestre

--- consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ConsumerApplication.java
+++ consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ConsumerApplication.java
@@ -3,11 +3,32 @@ package it.valeriovaudi.rsocket.workshop.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
+*/
+
+// TODO 4) enable RSocketRequester on client side
+
+
 @SpringBootApplication
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
+    /*
+    @Bean
+    public RSocketRequester rSocketRequester(RSocketRequester.Builder builder) {
+        int port = 7000;
+        String host = "localhost";
+
+        return builder
+                .dataMimeType(MediaType.APPLICATION_CBOR)
+                .connectTcp(host, port)
+                .block();
+    }
+    */
 }
