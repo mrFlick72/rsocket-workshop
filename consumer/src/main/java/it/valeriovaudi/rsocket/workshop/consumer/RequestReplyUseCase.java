66ec0d14b962070700b5ce6888162a166b641ffb
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-24 00:32:13 +0100

    req reply consumer solution

--- consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/RequestReplyUseCase.java
+++ consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/RequestReplyUseCase.java
@@ -1,34 +1,18 @@
 package it.valeriovaudi.rsocket.workshop.consumer;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-@RestController
-public class RequestReplyUseCase {
-
-    private final EchoUseCase echoUseCase;
-
-    public RequestReplyUseCase(EchoUseCase echoUseCase) {
-        this.echoUseCase = echoUseCase;
-    }
-
-    @PostMapping("/echo")
-    public Mono<String> echo(@RequestBody String message) {
-        return echoUseCase.echo(message);
-    }
-}
-
-@Service
-class EchoUseCase {
+@Component
+public class RequestReplyUseCase implements ApplicationRunner {
 
     private final Mono<RSocketRequester> requester;
 
-    public EchoUseCase(RSocketRequester.Builder builder) {
+    public RequestReplyUseCase(RSocketRequester.Builder builder) {
         int port = 7000;
         String host = "localhost";
 
@@ -37,9 +21,12 @@ class EchoUseCase {
                 .connectTcp(host, port);
     }
 
-    public Mono<String> echo(String message) {
-        return requester.flatMap(rSocketRequester -> rSocketRequester.route("route.request.reply")
-                .data(message)
-                .retrieveMono(String.class));
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        requester.flatMap(rSocketRequester -> rSocketRequester.route("route.request.reply")
+                .data("this is an echo message")
+                .retrieveMono(String.class))
+                .subscribe(System.out::println);
     }
 }
--- consumer/src/test/java/it/valeriovaudi/rsocket/workshop/consumer/EchoUseCaseTest.java
+++
@@ -1,7 +0,0 @@
-package it.valeriovaudi.rsocket.workshop.consumer;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EchoUseCaseTest {
-
-}
