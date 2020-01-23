cfab5c856ad6fabea1c4ac457d14e3ef45f47d72
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-24 00:41:06 +0100

    exercise 2 track

--- consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/FireAndForGetUseCase.java
+++ consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/FireAndForGetUseCase.java
@@ -1,4 +1,28 @@
 package it.valeriovaudi.rsocket.workshop.consumer;
 
-public class FireAndForGetUseCase {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FireAndForGetUseCase implements ApplicationRunner {
+
+    // TODO 4) enable RSocketRequester on client side
+    /*private final Mono<RSocketRequester> requester;
+
+    public FireAndForGetUseCase(RSocketRequester.Builder builder) {
+        int port = 7000;
+        String host = "localhost";
+
+        this.requester = builder
+                .dataMimeType(MediaType.APPLICATION_JSON)
+                .connectTcp(host, port);
+    }*/
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //TODO insert here your solution and pay attention to the log
+        // remember to start the publisher app before
+    }
 }
