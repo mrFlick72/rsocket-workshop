9c3339b1d41fc2db15fdf2a080024176365e76b6
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-26 00:21:40 +0100

    starting point

--- consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ChannelUseCase.java
+++ consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ChannelUseCase.java
@@ -3,13 +3,16 @@ package it.valeriovaudi.rsocket.workshop.consumer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
-@Component
+@Controller
 public class ChannelUseCase implements ApplicationRunner {
 
-    private final RSocketRequester requester;
+    private final Mono<RSocketRequester> requester;
 
     public ChannelUseCase(RSocketRequester.Builder builder) {
         int port = 7000;
@@ -18,19 +21,25 @@ public class ChannelUseCase implements ApplicationRunner {
         this.requester = builder
                 .dataMimeType(MediaType.APPLICATION_JSON)
                 .connectTcp(host, port)
-                .block();
+                .cache();
+    }
+
+    @MessageMapping("route.request.channel")
+    public Mono<Void> ackCollector(String ack) {
+        return Mono.just(ack).log().then();
     }
 
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        requester.route("route.request.channel")
-                .data("Hey! hello man!")
-                .retrieveFlux(String.class)
-                .take(10)
-                .log()
-                .then()
-                .block();
+        requester
+                .flatMap(req -> req.route("route.request.channel")
+                        .data("Hey! hello man!")
+                        .retrieveFlux(String.class)
+                        .take(10)
+                        .log()
+                        .then())
+                .subscribe();
 
     }
 }
--- publisher/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/ChannelUseCase.java
+++ publisher/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/ChannelUseCase.java
@@ -1,17 +1,37 @@
 package it.valeriovaudi.rsocket.workshop.publisher;
 
+import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @Controller
 public class ChannelUseCase {
 
+    private final Mono<RSocketRequester> requester;
+
+    public ChannelUseCase(RSocketRequester.Builder builder) {
+        int port = 7001;
+        String host = "localhost";
+
+        this.requester = builder
+                .dataMimeType(MediaType.APPLICATION_JSON)
+                .connectTcp(host, port)
+                .cache();
+    }
+
+
     @MessageMapping("route.request.channel")
     public Flux<String> streamEcho(String message) {
         return Flux.interval(Duration.ofSeconds(1))
-                .flatMap(tick -> Flux.just(message));
+                .flatMap(tick -> requester.flatMap(rSocketRequester -> rSocketRequester.route("route.request.channel")
+                        .data("message  fired")
+                        .send()
+                        .log())
+                        .thenMany(Flux.just(message)));
     }
 }
