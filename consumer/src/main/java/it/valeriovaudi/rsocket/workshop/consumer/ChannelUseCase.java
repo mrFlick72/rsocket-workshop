9d0e6a3c5d05ede832b7f44ef5c238e902ba6f8c
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-26 18:04:28 +0100

    backpressure example

--- consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ChannelUseCase.java
+++ consumer/src/main/java/it/valeriovaudi/rsocket/workshop/consumer/ChannelUseCase.java
@@ -3,15 +3,16 @@ package it.valeriovaudi.rsocket.workshop.consumer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+
+import java.util.Random;
 
 @Controller
 public class ChannelUseCase implements ApplicationRunner {
 
+    Random random = new Random();
     private final Mono<RSocketRequester> requester;
 
     public ChannelUseCase(RSocketRequester.Builder builder) {
@@ -24,22 +25,21 @@ public class ChannelUseCase implements ApplicationRunner {
                 .cache();
     }
 
-    @MessageMapping("route.request.channel")
-    public Mono<Void> ackCollector(String ack) {
-        return Mono.just(ack).log().then();
-    }
-
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         requester
                 .flatMap(req -> req.route("route.request.channel")
                         .data("Hey! hello man!")
-                        .retrieveFlux(String.class)
-                        .take(10)
+                        .retrieveFlux(Object.class)
                         .log()
+                        .flatMap(strings ->
+                                requester.flatMap(rSocketRequester ->
+                                        rSocketRequester.route("route.request.channel")
+                                                .data(random.nextInt(3) + 1)
+                                                .send())
+                        )
                         .then())
                 .subscribe();
-
     }
 }
