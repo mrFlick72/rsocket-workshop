86bc7505253f7888d8e04e25e64b29baa4c23850
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-31 00:35:25 +0100

    test cases

--- server/src/test/java/it/valeriovaudi/rsocket/workshop/server/task1/ReactorExerciseTest.java
+++ server/src/test/java/it/valeriovaudi/rsocket/workshop/server/task1/ReactorExerciseTest.java
@@ -2,6 +2,7 @@ package it.valeriovaudi.rsocket.workshop.server.task1;
 
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -17,4 +18,46 @@ public class ReactorExerciseTest {
 
         anEagerMono.subscribe();
     }
+
+    @Test
+    public void lazyMono() {
+        Mono<String> anEagerMono = ReactorExercise.lazyMono();
+
+        StepVerifier.create(anEagerMono)
+                .expectNext("an eager mono")
+                .verifyComplete();
+    }
+
+
+    @Test
+    public void firstTenElementOfAnInfiniteFlux() {
+        Flux<String> firstTenElementOfAnInfiniteFlux = ReactorExercise.firstTenElementOfAnInfiniteFlux();
+
+        StepVerifier.create(firstTenElementOfAnInfiniteFlux)
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .expectNext("an eager mono")
+                .verifyComplete();
+    }
+
+    @Test
+    public void composableFlux() {
+        Flux<String> firstTenElementOfAnInfiniteFlux =
+                ReactorExercise.composableFlux(Flux.just("a", "b", "c"), Flux.just("c", "b", "a"));
+
+        StepVerifier.create(firstTenElementOfAnInfiniteFlux)
+                .expectNext("ac")
+                .expectNext("bb")
+                .expectNext("ca")
+                .verifyComplete();
+    }
+
+
 }
