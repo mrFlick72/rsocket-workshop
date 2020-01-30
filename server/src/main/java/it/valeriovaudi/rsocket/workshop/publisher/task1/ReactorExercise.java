95148127facd075f38d2b538f991bc9215581be3
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-30 23:48:44 +0100

    WIP

--- server/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/task1/ReactorExercise.java
+++ server/src/main/java/it/valeriovaudi/rsocket/workshop/publisher/task1/ReactorExercise.java
@@ -1,4 +1,24 @@
 package it.valeriovaudi.rsocket.workshop.publisher.task1;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class ReactorExercise {
+
+    public static Mono eagerMono(){
+        return null;
+    }
+
+    public static Mono lazyMono(){
+        return null;
+    }
+
+    public static Flux firstTenElementOfAnInfiniteFlux(){
+        return null;
+    }
+
+    public static Flux composableFlux(Flux<String> aFlux, Flux<String> anotherFlux){
+        return null;
+    }
+
 }
