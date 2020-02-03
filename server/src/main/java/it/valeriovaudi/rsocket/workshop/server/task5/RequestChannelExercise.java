727c041df0247c7653d80725b1487683ad53da69
Valerio Vaudi <valerio.vaudi@lastminute.com>
2020-02-03 09:06:16 +0100

    fix

--- server/src/main/java/it/valeriovaudi/rsocket/workshop/server/task5/RequestChannelExercise.java
+++ server/src/main/java/it/valeriovaudi/rsocket/workshop/server/task5/RequestChannelExercise.java
@@ -1,13 +1,16 @@
 package it.valeriovaudi.rsocket.workshop.server.task5;
 
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class RequestChannelExercise {
 
-/*    private final Mono<RSocketRequester> requester;
+    private final Mono<RSocketRequester> requester;
 
-    public ChannelUseCase(RSocketRequester.Builder builder) {
+    public RequestChannelExercise(RSocketRequester.Builder builder) {
         int port = 7001;
         String host = "localhost";
 
@@ -15,6 +18,5 @@ public class RequestChannelExercise {
                 .dataMimeType(MediaType.APPLICATION_JSON)
                 .connectTcp(host, port)
                 .cache();
-    }*/
-
+    }
 }
